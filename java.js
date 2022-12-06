df2d696224842ed1e1e150f9b346b5366d0b682b
Jopdyke1624 <jopdyke1624@gmail.com>
2022-12-05 18:15:25 -0600

    change js to function better

--- java.js
+++ java.js
@@ -1,44 +1,99 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
-    
-    const fetchTodos = () => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-        .then( (response) => response.json())
-        .then( (json) => arrayOfTodos = json)
-        
-    }
-    
-    const logTodos = () => {
-        // console.log(arrayOfTodos[0].title)
-        // const toDo = document.getElementById('todo-list')
-        // const list = document.createElement("li")
-        // const node = document.createTextNode(arrayOfTodos[0].title)
-        // list.appendChild(node)
-        // toDo.appendChild(list)
+let arrayOfTodos = [
+  {
+    userId: 14,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 20,
+    id: 2,
+    title: "delectus aut autem",
+    completed: false,
+  },
+];
 
+const toDo = document.getElementById("todo-list");
 
-    }
-    
-    const populateTodos = () => {
-        console.log(arrayOfTodos[0].title)
-        const toDo = document.getElementById('todo-list')
-        const list = document.createElement("li")
-        const title = document.createTextNode(arrayOfTodos[0].title)
-        list.appendChild(title)
-        toDo.appendChild(list)
-    }
+const fetchTodos = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => (arrayOfTodos = json));
+};
 
-  
+const userInput = () => {
+  const userId = document.getElementById("userId").value;
+  toDo.innerHTML = "";
+
+  // const filtered =
+  arrayOfTodos
+    .filter((element) => {
+      return +userId === element.userId;
+    })
+
+    .forEach(() => {
+      const list = document.createElement("li");
+      list.innerHTML = `
+                <p>userId: ${el.userId}</p>
+                <p>todo:${el.title}</p>
+                <p>completed:${el.completed}</p>
+            `;
+      toDo.appendChild(list);
+    });
+
+  // arrayOfTodos.forEach((el)=>{
+  //     if(+userId === el.userId){
+  //         const list = document.createElement("li")
+  //         list.innerHTML = `
+  //             <p>userId: ${el.userId}</p>
+  //             <p>todo:${el.title}</p>
+  //             <p>completed:${el.completed}</p>
+  //         `
+  //         toDo.appendChild(list)
+  //     }
+  // })
+
+  // const filtered = arrayOfTodos.filter((element)=>{
+  //     return +userId === element.userId
+  // })
+  // console.log(filtered)
+
+  // for (let index = 0; index <filtered.length; index++) {
+  //     const element =filtered[index];
+  //     console.log(userId, element.userId)
+  //         console.log(element)
+
+  //         const list = document.createElement("li")
+  //         list.innerHTML = `
+  //             <p>userId: ${element.userId}</p>
+  //             <p>todo:${element.title}</p>
+  //             <p>completed:${element.completed}</p>
+  //         `
+  //         toDo.appendChild(list)
+  // }
+};
+
+const logTodos = () => {
+  console.log(arrayOfTodos);
+};
+
+const populateTodos = () => {
+  toDo.innerHTML = "";
+  for (let index = 0; index < arrayOfTodos.length; index++) {
+    const element = arrayOfTodos[index];
+    const list = document.createElement("li");
+    const title = document.createTextNode(element.title);
+
+    list.appendChild(title);
+    toDo.appendChild(list);
+  }
+};
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // ol.innerHTML = ""
+});
